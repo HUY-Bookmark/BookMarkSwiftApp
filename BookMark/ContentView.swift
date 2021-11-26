4200bd0af07125d10c4073ecb06a008cb8243cf5
Sarah <sschlegel@myges.fr>
2021-11-27 00:38:30 +0900

    fetch bookshelf complete data with book list

--- BookMark/ContentView.swift
+++ BookMark/ContentView.swift
@@ -2,21 +2,65 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var results = [Test]()
+    @State var bookshelves = [Bookshelf]()
+    @State var bookList = [Test]()
     
-    var apiURL = "http://localhost:8888/api/book/search/Harry/ink";
+    var usrData = UsrData(usr: 1, tok: "19208310")
+    
+    var bookshelfApiURL = "http://localhost:8888/api/bookshelf"
+    var bookIdApiURL = "http://localhost:8888/api/book/id/"
   
     var body: some View {
-        List(results, id: \.id) { item in
+        List(bookList, id: \.id) { item in
             VStack(alignment: .leading) {
-                Text(fullTitle(book: item))
+                Text(fullTitle(book:item))
             }
         }.onAppear(perform: loadData)
     }
     
   
     func loadData() {
-        guard let url: URL = URL(string: self.apiURL)
+        guard let url: URL = URL(string: self.bookshelfApiURL)
+        else {
+            print("invalid URL")
+            return
+        }
+        
+        var urlRequest: URLRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
+        do {
+            urlRequest.httpBody = try JSONEncoder().encode(usrData)
+            //print(String(data: urlRequest.httpBody!, encoding: .utf8)!)
+            URLSession.shared.dataTask(with: urlRequest, completionHandler: { (data, response, error) in
+                // check if response is okay
+                
+                guard let data = data else {
+                    print("invalid response")
+                    return
+                }
+                
+                // convert JSON response into class model as an array
+                do {
+                    self.bookshelves = try JSONDecoder().decode([Bookshelf].self, from: data)
+                    var i = 1
+                    for bookshelf in self.bookshelves {
+                        for shelf in bookshelf.shelves {
+                            fetchBookData(bookid: shelf.book_id, id: i)
+                            i += 1
+                        }
+                    }
+                } catch {
+                    print("Catch " + error.localizedDescription)
+                }
+                
+            }).resume()
+        } catch {
+            print("Catch" + error.localizedDescription)
+        }
+    }
+    
+    func fetchBookData (bookid: String, id: Int) {
+        guard let url: URL = URL(string: self.bookIdApiURL + bookid)
         else {
             print("invalid URL")
             return
@@ -24,6 +68,7 @@ struct ContentView: View {
         
         var urlRequest: URLRequest = URLRequest(url: url)
         urlRequest.httpMethod = "GET"
+        
         URLSession.shared.dataTask(with: urlRequest, completionHandler: { (data, response, error) in
             // check if response is okay
             
@@ -32,11 +77,11 @@ struct ContentView: View {
                 return
             }
             
-            print(data)
-            
             // convert JSON response into class model as an array
             do {
-                self.results = try JSONDecoder().decode([Test].self, from: data)
+                var books = try JSONDecoder().decode([Test].self, from: data)
+                books[0].id = id
+                bookList.append(books[0])
             } catch {
                 print("Catch " + error.localizedDescription)
             }
@@ -45,7 +90,8 @@ struct ContentView: View {
     }
     
     func fullTitle (book:Test) -> String {
-        return book.series_name + " #" + String(book.series_position) + ": " + book.book_name;
+        return book.series_name + " #" + String(book.series_position)
+        + ": " + book.book_name
     }
     
 }
--- BookMark/Model.swift
+++ BookMark/Model.swift
@@ -7,13 +7,34 @@
 
 import Foundation
 
+struct UsrData: Codable {
+    var usr: Int
+    var tok: String
+}
+
 struct Test: Codable {
     let response: String
-    let id: Int
+    var id: Int
     let isbn13: String
     let isbn11: String
     let book_name: String
     let series_name: String
     let series_position: Int
     let genre: String
+}
+
+struct Shelf: Codable {
+    let book_id: String
+    let shelf_number: Int
+    let shelf_position: Int
+}
+
+struct Bookshelf: Codable {
+    let id: Int
+    let width: Int
+    let height: Int
+    let depth: Int
+    let shelves_number: Int
+    let sorting: String
+    let shelves: [Shelf]
 }
